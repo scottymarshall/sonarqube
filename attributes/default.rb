2cc3ed8c0298da432fe05a2265d7b244546eb278
105325 <scott.marshall@johnmuirhealth.com>
2018-04-06 15:04:12 -0700

    upgrade to 7.0

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,6 +1,6 @@
 default['sonarqube']['mirror'] = 'https://sonarsource.bintray.com/Distribution/sonarqube/'
-default['sonarqube']['version'] = '5.1.2'
-default['sonarqube']['checksum'] = 'a8d63d837242d0d07c0b3f65cfa9c84d5ae82ee51c6cbb52248bcf0d1bc58491'
+default['sonarqube']['version'] = '7.0'
+default['sonarqube']['checksum'] = '263942458279e2cf73fd86671511ac8ef0707e41ed51c9737142f8738bc7c060'
 default['sonarqube']['os_kernel'] = 'linux-x86-64'
 
 default['sonarqube']['config']['dir'] = '/opt/sonarqube-%{version}/conf'
@@ -28,18 +28,6 @@ default['sonarqube']['jdbc']['maxWait'] = 5000
 default['sonarqube']['jdbc']['minEvictableIdleTimeMillis'] = 600_000
 default['sonarqube']['jdbc']['timeBetweenEvictionRunsMillis'] = 30_000
 
-default['sonarqube']['web']['https']['keyAlias'] = nil
-default['sonarqube']['web']['https']['keyPass'] = 'changeit'
-default['sonarqube']['web']['https']['keystoreFile'] = nil
-default['sonarqube']['web']['https']['keystorePass'] = nil
-default['sonarqube']['web']['https']['keystoreType'] = 'JKS'
-default['sonarqube']['web']['https']['keystoreProvider'] = nil
-default['sonarqube']['web']['https']['truststoreFile'] = nil
-default['sonarqube']['web']['https']['truststorePass'] = nil
-default['sonarqube']['web']['https']['truststoreType'] = 'JKS'
-default['sonarqube']['web']['https']['truststoreProvider'] = nil
-default['sonarqube']['web']['https']['clientAuth'] = false
-
 default['sonarqube']['web']['http']['maxThreads'] = 50
 default['sonarqube']['web']['http']['minThreads'] = 5
 default['sonarqube']['web']['http']['acceptCount'] = 25
@@ -48,7 +36,6 @@ default['sonarqube']['web']['https']['maxThreads'] = 50
 default['sonarqube']['web']['https']['acceptCount'] = 25
 
 default['sonarqube']['web']['accessLogs']['enable'] = true
-default['sonarqube']['ajp']['port'] = 9009
 
 default['sonarqube']['updatecenter']['activate'] = true
 default['sonarqube']['http']['proxyHost'] = nil
@@ -58,10 +45,7 @@ default['sonarqube']['socksProxyHost'] = nil
 default['sonarqube']['socksProxyPort'] = nil
 default['sonarqube']['http']['proxyUser'] = nil
 default['sonarqube']['http']['proxyPassword'] = nil
-default['sonarqube']['notifications']['delay'] = 60
-default['sonarqube']['log']['profilingLevel'] = 'NONE'
-
-default['sonarqube']['rails']['dev'] = false
+default['sonarqube']['log']['profilingLevel'] = 'INFO'
 
 default['sonarqube']['extra_properties'] = [
   # 'sonar.security.realm=LDAP',
